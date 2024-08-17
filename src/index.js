cbc8f6f76193a2514aaa5929a717cf57749071d1
Danil Chesnokov <shaytann831@gmail.com>
2024-08-17 12:55:40 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,26 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let res = '';
+    let oneCharacter;
+    for (let i = 0; i < expr.length; i+= 10){
+        oneCharacter = '';
+        for (let j = i + 1; j <= i + 9; j += 2){
+            if ((expr[j - 1] + expr[j]) === '**') {
+                break;
+            } else if ((expr[j - 1] + expr[j]) === '10'){
+                oneCharacter += '.';
+            } else if ((expr[j - 1] + expr[j]) === '11'){
+                oneCharacter += '-'
+            }
+        }
+        if (oneCharacter === '') {
+            res += ' ';
+        } else {
+            res += MORSE_TABLE[oneCharacter];
+        }
+    }
+    return res;
 }
 
 module.exports = {
